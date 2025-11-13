f49c776d032a848b87ae97b6bf861b4374665bb4
kimminseok <alstjr706@naver.com>
2025-11-13 17:50:39 +0900

    Initial commit

---
+++ src/main/java/com/example/shared_todo/SharedTodoApplication.java
@@ -0,0 +1,13 @@
+package com.example.shared_todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharedTodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SharedTodoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/shared_todo/SharedTodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shared_todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharedTodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
