6806ab79c16fd1682a4eb940125c7963a61622cd
kimminseok <alstjr706@naver.com>
2025-11-15 21:08:07 +0900

    feat: tagId로 연결된 Todo ID 목록 조회 쿼리 메서드 추가

--- src/main/java/com/example/shared_todo/todotag/repository/TodoTagRepository.java
+++ src/main/java/com/example/shared_todo/todotag/repository/TodoTagRepository.java
@@ -3,6 +3,8 @@ package com.example.shared_todo.todotag.repository;
 import com.example.shared_todo.todotag.entity.TodoTag;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -12,4 +14,7 @@ public interface TodoTagRepository extends JpaRepository<TodoTag, Long> {
     void deleteByTodoId(Long todoId);
 
     List<TodoTag> findByTodoId(Long todoId);
+
+    @Query("SELECT DISTINCT tt.todo.id FROM TodoTag tt WHERE tt.tag.id = :tagId")
+    List<Long> findTodoIdsByTagId(@Param("tagId") Long tagId);
 }
