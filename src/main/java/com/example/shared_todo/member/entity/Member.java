f52f8c5a5b2a27f13491615398182e50c38c19fa
kimminseok <alstjr706@naver.com>
2025-11-15 12:33:20 +0900

    feat: Member 엔티티 추가

---
+++ src/main/java/com/example/shared_todo/member/entity/Member.java
@@ -0,0 +1,39 @@
+package com.example.shared_todo.member.entity;
+
+import com.example.shared_todo.common.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Member extends BaseEntity {
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "nickname", nullable = false)
+    private String nickname;
+
+    @Builder
+    public Member(String email, String password, String nickname) {
+        this.email = email;
+        this.password = password;
+        this.nickname = nickname;
+    }
+
+    public static Member create(String email, String nickname, String encodedPassword) {
+        return Member.builder()
+                .email(email)
+                .nickname(nickname)
+                .password(encodedPassword)
+                .build();
+    }
+}
