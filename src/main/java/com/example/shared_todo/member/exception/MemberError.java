008f8b99257f133d2ce2dbc4205054d17b37551a
kimminseok <alstjr706@naver.com>
2025-11-15 12:44:08 +0900

    feat: 회원 도메인 예외 코드 및 MemberException 추가

---
+++ src/main/java/com/example/shared_todo/member/exception/MemberError.java
@@ -0,0 +1,19 @@
+package com.example.shared_todo.member.exception;
+
+import com.example.shared_todo.common.exception.CommonError;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum MemberError implements CommonError {
+
+    EMAIL_DUPLICATED(HttpStatus.BAD_REQUEST, "40001", "이미 등록된 이메일입니다."),
+    EMAIL_NOT_FOUND(HttpStatus.NOT_FOUND, "40402", "존재하지 않는 회원 이메일입니다."),
+    PASSWORD_NOT_MATCHED(HttpStatus.BAD_REQUEST, "40002", "비밀번호가 일치하지 않습니다.");
+
+    private final HttpStatus httpStatus;
+    private final String code;
+    private final String message;
+}
