82e2cde1c38f43978826f74eaa7fab2fa7d96117
kimminseok <alstjr706@naver.com>
2025-11-15 14:10:07 +0900

    feat: 로그인 인증 및 JWT 발급 로직 구현

--- src/main/java/com/example/shared_todo/member/repository/MemberRepository.java
+++ src/main/java/com/example/shared_todo/member/repository/MemberRepository.java
@@ -3,7 +3,11 @@ package com.example.shared_todo.member.repository;
 import com.example.shared_todo.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
     boolean existsByEmail(String email);
+
+    Optional<Member> findByEmail(String email);
 }
--- src/main/java/com/example/shared_todo/member/service/MemberService.java
+++ src/main/java/com/example/shared_todo/member/service/MemberService.java
@@ -1,10 +1,14 @@
 package com.example.shared_todo.member.service;
 
+import com.example.shared_todo.common.jwt.JwtToken;
+import com.example.shared_todo.common.jwt.TokenProvider;
 import com.example.shared_todo.member.entity.Member;
 import com.example.shared_todo.member.exception.MemberError;
 import com.example.shared_todo.member.exception.MemberException;
 import com.example.shared_todo.member.repository.MemberRepository;
+import com.example.shared_todo.member.service.dto.request.SignInRequest;
 import com.example.shared_todo.member.service.dto.request.SignUpRequest;
+import com.example.shared_todo.member.service.dto.response.JwtTokenResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -15,6 +19,7 @@ import org.springframework.transaction.annotation.Transactional;
 public class MemberService {
 
     private final MemberRepository memberRepository;
+    private final TokenProvider tokenProvider;
     private final PasswordEncoder passwordEncoder;
 
     @Transactional
@@ -30,5 +35,18 @@ public class MemberService {
                 encodedPassword
         );
         memberRepository.save(member);
+    }
+
+    @Transactional(readOnly = true)
+    public JwtTokenResponse signIn(SignInRequest request) {
+        Member member = memberRepository.findByEmail(request.getEmail())
+                .orElseThrow(() -> new MemberException(MemberError.EMAIL_NOT_FOUND));
+
+        if (!passwordEncoder.matches(request.getPassword(), member.getPassword())) {
+            throw new MemberException(MemberError.PASSWORD_NOT_MATCHED);
+        }
+
+        JwtToken token = tokenProvider.generateToken(member.getId());
+        return JwtTokenResponse.from(token);
     }
 }
