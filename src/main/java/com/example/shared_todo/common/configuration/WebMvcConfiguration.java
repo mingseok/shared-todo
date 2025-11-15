883b977020fef4a978ed33b7dd8df0efa1496c3f
kimminseok <alstjr706@naver.com>
2025-11-15 12:44:35 +0900

    chore: 인증 ArgumentResolver(WebMvc) 등록

--- src/main/java/com/example/shared_todo/common/configuration/WebMvcConfiguration.java
+++ src/main/java/com/example/shared_todo/common/configuration/WebMvcConfiguration.java
@@ -1,11 +1,15 @@
 package com.example.shared_todo.common.configuration;
 
+import com.example.shared_todo.common.identity.resolver.AuthMemberIdArgumentResolver;
 import com.example.shared_todo.common.interceptor.JwtAuthInterceptor;
 import com.example.shared_todo.common.jwt.TokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @Configuration
 @RequiredArgsConstructor
@@ -15,7 +19,13 @@ public class WebMvcConfiguration implements WebMvcConfigurer {
     public static final String PUBLIC_API_PREFIX = "/api/public/**";
     public static final String VIEW_PAGE_PREFIX = "/view/**";
 
+    private final AuthMemberIdArgumentResolver authResolver;
     private final TokenProvider tokenProvider;
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(authResolver);
+    }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
